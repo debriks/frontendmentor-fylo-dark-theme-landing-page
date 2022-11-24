4fefdb2aaf55d47ae2c4be5c53e997dddf469ecf
Debriks <deborah.robbiano@gmail.com>
2022-11-24 23:50:05 +0100

    HTML / SCSS / JS/Responsive

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = form["email"];
+  const emailValue = email.value;
+  const small = form.querySelector("small");
+
+  if (!emailValue) {
+    // input value is empty
+    small.innerText = "Please provide a valid email address";
+    small.style.display = "inline-block";
+    // input value is invalid
+  } else if (!isValidEmail(emailValue)) {
+    small.innerText = "Please provide a valid email address";
+    small.style.display = "inline-block";
+  } else {
+    // input value is correct
+    email.classList.remove("error");
+    small.innerText = "";
+    small.style.display = "none";
+  }
+});
+
+// function that checks if email is valid
+function isValidEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+console.log("Hello World");
